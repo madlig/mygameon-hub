08af6bdbefef4c5d80c1efa459ed9e3b50fe55ec
madlig <madlighifari29@gmail.com>
2026-05-11 15:15:23 +0700

    fix: deleting get-token

--- app/api/get-token/route.js
+++
@@ -1,9 +0,0 @@
-import { auth } from '@/app/api/auth/[...nextauth]/route'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  const session = await auth()
-  return NextResponse.json({
-    refreshToken: session?.refreshToken || null
-  })
-}
